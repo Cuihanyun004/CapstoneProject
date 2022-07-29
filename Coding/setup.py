c8a3c2fe70054e4165cd8fa9b741201d704e8b5f
CUI HANYUN <109054627+Cuihanyun004@users.noreply.github.com>
2022-07-29 19:25:10 +0800

    Create setup.py

---
+++ Coding/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="CapstoneProject",
+  ayther="Hanyun Cui"
+  auther-email="hc3370@columbia.edu"
+  license="MIT license",
+  description="Thia package provides AI-driven solutuins.",
+  packages=["Coding"]
+)    
